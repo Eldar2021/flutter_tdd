3c49d4b33f3d7c3cc8a132a936ad0d71f617a464
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-04-20 15:51:16 +0600

    tested NumberTriviaLocalDataSourceImpl

--- lib/features/number_trivia/data/sourses/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/sourses/number_trivia_local_data_source.dart
@@ -1,7 +1,32 @@
+import 'package:hive/hive.dart';
+import 'package:test_driven_development/core/error/exceptions.dart';
+
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
 
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  NumberTriviaLocalDataSourceImpl(this.hiveBox);
+
+  final Box<NumberTriviaModel> hiveBox;
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final val = hiveBox.get('Trivia');
+    if (val != null) {
+      return Future.value(val);
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) async {
+    await hiveBox.put('Trivia', triviaToCache);
+    return;
+  }
 }
---
+++ test/features/number_trivia/data/source/number_trivia_local_data_souce_test.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test_driven_development/core/error/exceptions.dart';
+import 'package:test_driven_development/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:test_driven_development/features/number_trivia/data/sourses/number_trivia_local_data_source.dart';
+
+class MockHiveBox extends Mock implements Box<NumberTriviaModel> {}
+
+void main() {
+  late MockHiveBox mockHiveBox;
+  late NumberTriviaLocalDataSource dataSource;
+  late NumberTriviaModel numberTriviaModel;
+
+  setUp(() {
+    mockHiveBox = MockHiveBox();
+    dataSource = NumberTriviaLocalDataSourceImpl(mockHiveBox);
+    numberTriviaModel = const NumberTriviaModel(
+      text: 'model text',
+      number: 1,
+    );
+  });
+
+  group(
+    'get tast number trivia',
+    () {
+      test(
+        'number trivia fron hive box',
+        () async {
+          when<NumberTriviaModel>(() => mockHiveBox.get('Trivia')!)
+              .thenReturn(numberTriviaModel);
+
+          final result = await dataSource.getLastNumberTrivia();
+
+          verify(() => mockHiveBox.get('Trivia'));
+
+          expect(result, numberTriviaModel);
+        },
+      );
+
+      test(
+        'null fron hive box',
+        () async {
+          when<dynamic>(() => mockHiveBox.get('Trivia')!).thenReturn(null);
+
+          final call = dataSource.getLastNumberTrivia;
+
+          expect(call, throwsA(const TypeMatcher<CacheException>()));
+        },
+      );
+    },
+  );
+
+  group('cache number trivia', () {
+    test(
+      'cache number trivia success',
+      () async {
+        when(() => mockHiveBox.put('Trivia', numberTriviaModel))
+            .thenAnswer((_) => Future.value());
+
+        await dataSource.cacheNumberTrivia(numberTriviaModel);
+
+        verify(() => mockHiveBox.put('Trivia', numberTriviaModel));
+      },
+    );
+  });
+}
